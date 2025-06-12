72f5a07e67e6227f2a66a44b35edcabc458e196a
sakshi26tangar <sakshitangar@gmail.com>
2025-06-12 14:45:44 +0530

    Basic

---
+++ Day-01_ProgrammingElements/Basic/brizlabs.java
@@ -0,0 +1,7 @@
+import java.util.*;
+public class brizlabs {
+    public static void main(String[] args) {
+        System.out.println("Welcome to BridgeLabz");
+    }
+    
+}
--- Day-04/KmToMilesConverter.java
+++
@@ -1,9 +0,0 @@
-public class KmToMilesConverter {
-    public static void main(String[] args) {
-        double kilometers = 10.8;
-        double conversionRate = 1.6;
-        double miles = kilometers * conversionRate;
-
-        System.out.println("The distance " + kilometers + " km in miles is " + miles);
-    }
-}
