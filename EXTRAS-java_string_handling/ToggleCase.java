feb0eedb280ad660558072b974e5781f2e1fee73
sakshi26tangar <sakshitangar@gmail.com>
2025-06-21 13:08:06 +0530

    [Sakshi Tangar] add . Strings handling code ToggleCase added

---
+++ EXTRAS-java_string_handling/ToggleCase.java
@@ -0,0 +1,14 @@
+public class ToggleCase {
+    public static String toggleCase(String str) {
+        StringBuilder result = new StringBuilder();
+        for (char c : str.toCharArray()) {
+            if (Character.isUpperCase(c))
+                result.append(Character.toLowerCase(c));
+            else if (Character.isLowerCase(c))
+                result.append(Character.toUpperCase(c));
+            else
+                result.append(c);
+        }
+        return result.toString();
+    }
+}
