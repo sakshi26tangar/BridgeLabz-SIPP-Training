715aad46093c0e30608ef31f275fed9cf4bde0a2
sakshi26tangar <sakshitangar@gmail.com>
2025-06-21 13:08:56 +0530

    [Sakshi Tangar] add . Strings handling code VowelConsonantCounter added

---
+++ EXTRAS-java_string_handling/VowelConsonantCounter.java
@@ -0,0 +1,15 @@
+public class VowelConsonantCounter {
+    public static void countVowelsAndConsonants(String str) {
+        int vowels = 0, consonants = 0;
+        str = str.toLowerCase();
+        for (char c : str.toCharArray()) {
+            if (Character.isLetter(c)) {
+                if ("aeiou".indexOf(c) != -1)
+                    vowels++;
+                else
+                    consonants++;
+            }
+        }
+        System.out.println("Vowels: " + vowels + ", Consonants: " + consonants);
+    }
+}
