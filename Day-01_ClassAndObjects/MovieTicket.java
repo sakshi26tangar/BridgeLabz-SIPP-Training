118b35c5adb24bf029d7b3c9ddcd5173017e2d07
sakshi26tangar <sakshitangar@gmail.com>
2025-06-24 15:27:10 +0530

    [Sakshi Tangar] add . OOPs code MovieTicket added

---
+++ Day-01_ClassAndObjects/MovieTicket.java
@@ -0,0 +1,24 @@
+class MovieTicket {
+    String movieName;
+    int seatNumber;
+    double price;
+
+    void bookTicket(String movieName, int seatNumber, double price) {
+        this.movieName = movieName;
+        this.seatNumber = seatNumber;
+        this.price = price;
+        System.out.println("Ticket booked successfully!");
+    }
+
+    void displayTicketDetails() {
+        System.out.println("Movie: " + movieName);
+        System.out.println("Seat Number: " + seatNumber);
+        System.out.println("Price: ₹" + price);
+    }
+
+    public static void main(String[] args) {
+        MovieTicket ticket = new MovieTicket();
+        ticket.bookTicket("Avengers: Endgame", 25, 250);
+        ticket.displayTicketDetails();
+    }
+}
