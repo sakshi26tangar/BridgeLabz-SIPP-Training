5ebe10128c17402b7dc62a9bdb057c742b479ca9
sakshi26tangar <sakshitangar@gmail.com>
2025-06-25 21:58:36 +0530

    [Sakshi Tangar] add . constractor code circle added

---
+++ Day-02_ConstructorsAndAccessModifier/Circle.java
@@ -0,0 +1,24 @@
+public class Circle {
+    double radius;
+
+    // Default constructor
+    public Circle() {
+        this(1.0); // Calls parameterized constructor
+    }
+
+    // Parameterized constructor
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public void display() {
+        System.out.println("Radius: " + radius);
+    }
+
+    public static void main(String[] args) {
+        Circle c1 = new Circle();
+        Circle c2 = new Circle(5.5);
+        c1.display();
+        c2.display();
+    }
+}
