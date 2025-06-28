4605fb62008bb6e85ea93d2fab3fd984a7531063
sakshi26tangar <sakshitangar@gmail.com>
2025-06-28 18:04:01 +0530

    clean

--- EXTRAS-java_string_handling/WordReplace.java
+++
@@ -1,5 +0,0 @@
-public class WordReplace {
-    public static String replaceWord(String sentence, String oldWord, String newWord) {
-        return sentence.replaceAll("\\b" + oldWord + "\\b", newWord);
-    }
-}
