407951e3648c72193785e40795a6e6db3eea996f
sakshi26tangar <sakshitangar@gmail.com>
2025-07-07 21:43:38 +0530

    [Sakshi Tangar] add . Two Sum Problem added

---
+++ Day-02_Stack-Queue-Hash/TwoSum.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class TwoSum {
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int comp = target - nums[i];
+            if (map.containsKey(comp)) {
+                return new int[]{map.get(comp), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[]{-1, -1}; // No pair found
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = twoSum(arr, target);
+        System.out.println("Indices: " + result[0] + ", " + result[1]);
+    }
+}
