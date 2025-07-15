ce3fd2f66eb93f64fb29b21593d367188cf4199b
sakshi26tangar <sakshitangar@gmail.com>
2025-07-15 10:18:55 +0530

    [Sakshi Tangar] add . class BusinessWallet extends Wallet added

---
+++ Workshop_03/BusinessWallet.java
@@ -0,0 +1,18 @@
+public class BusinessWallet extends Wallet {
+    private static final double LIMIT = 100000;
+
+    public BusinessWallet(double bonus) {
+        super(bonus);
+    }
+
+    @Override
+    public void transferTo(User receiver, double amount) {
+        if (amount <= getBalance() && amount <= LIMIT) {
+            deduct(amount);
+            receiver.wallet.credit(amount);
+            history.add(new Transaction("Transferred to " + receiver.name, amount));
+        } else {
+            System.out.println("Transfer failed: Limit or balance exceeded.");
+        }
+    }
+}
