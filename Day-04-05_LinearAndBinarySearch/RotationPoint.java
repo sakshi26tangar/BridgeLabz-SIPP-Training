547e3f8796ae81272b76ec9481fed36046802b51
sakshi26tangar <sakshitangar@gmail.com>
2025-07-16 12:09:21 +0530

    [Sakshi Tangar] add . Binary Search Rotation Point in a Rotated Sorted Array added

---
+++ Day-04-05_LinearAndBinarySearch/RotationPoint.java
@@ -0,0 +1,30 @@
+public class RotationPoint {
+
+    public static int findRotationPoint(int[] arr) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            
+            if (arr[mid] > arr[right]) {
+                left = mid + 1;
+            } else {
+                
+                right = mid;
+            }
+        }
+
+        
+        return left;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {15, 18, 2, 3, 6, 12};  // Rotated sorted array
+        int index = findRotationPoint(arr);
+
+        // System.out.println("The smallest element is at index: " + index);
+        // System.out.println("The smallest element is: " + arr[index]);
+    }
+}
