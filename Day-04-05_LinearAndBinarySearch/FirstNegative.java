2cb66a84237a1a2f5fec2ff4d857974d9f578f48
sakshi26tangar <sakshitangar@gmail.com>
2025-07-16 12:23:50 +0530

    [Sakshi Tangar] add . Linear Search for the First Negative Number added

--- Day-04-05_LinearAndBinarySearch/FirstNegative.java
+++ Day-04-05_LinearAndBinarySearch/FirstNegative.java
@@ -1,8 +1,22 @@
 public class FirstNegative {
+
     public static int findFirstNegative(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] < 0) return i;
+            if (arr[i] < 0) {
+                return i;
+            }
         }
-        return -1;
+        return -1; // No negative number found
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5, 10, 3, -7, 2, -1};
+        int index = findFirstNegative(arr);
+
+        if (index != -1) {
+            System.out.println("First negative number is at index: " + index);
+        } else {
+            System.out.println("No negative number found.");
+        }
     }
 }
