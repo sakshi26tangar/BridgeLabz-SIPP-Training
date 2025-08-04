bfde357c9abebffcef62158a59ad0941cf6273e0
sakshi26tangar <sakshitangar@gmail.com>
2025-08-04 11:14:38 +0530

    [Sakshi Tangar] Read a Large File Line by Line added

--- Day-03_JavaStreams/ErrorLineReader.java
+++ Day-03_JavaStreams/ErrorLineReader.java
@@ -3,7 +3,6 @@ import java.io.*;
 public class ErrorLineReader {
     public static void main(String[] args) {
         File file = new File("largefile.txt");
-
         try (BufferedReader br = new BufferedReader(new FileReader(file))) {
             String line;
             while ((line = br.readLine()) != null) {
