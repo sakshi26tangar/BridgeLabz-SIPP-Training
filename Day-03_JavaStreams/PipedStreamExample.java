9bd51f26802e1a64a30c6ced6a9dd57b2ffc356c
sakshi26tangar <sakshitangar@gmail.com>
2025-08-04 10:59:22 +0530

    [Sakshi Tangar] Piped Streams - Inter-Thread Communication added

---
+++ Day-03_JavaStreams/PipedStreamExample.java
@@ -0,0 +1,59 @@
+import java.io.*;
+
+class WriterThread extends Thread {
+    private PipedOutputStream pos;
+
+    public WriterThread(PipedOutputStream pos) {
+        this.pos = pos;
+    }
+
+    public void run() {
+        try {
+            String message = "Hello from WriterThread!";
+            pos.write(message.getBytes());
+            pos.close();
+        } catch (IOException e) {
+            System.out.println("WriterThread IOException: " + e.getMessage());
+        }
+    }
+}
+
+class ReaderThread extends Thread {
+    private PipedInputStream pis;
+
+    public ReaderThread(PipedInputStream pis) {
+        this.pis = pis;
+    }
+
+    public void run() {
+        try {
+            int data;
+            while ((data = pis.read()) != -1) {
+                System.out.print((char) data);
+            }
+            pis.close();
+        } catch (IOException e) {
+            System.out.println("ReaderThread IOException: " + e.getMessage());
+        }
+    }
+}
+
+public class PipedStreamExample {
+    public static void main(String[] args) {
+        try {
+            PipedOutputStream pos = new PipedOutputStream();
+            PipedInputStream pis = new PipedInputStream(pos);
+
+            WriterThread writer = new WriterThread(pos);
+            ReaderThread reader = new ReaderThread(pis);
+
+            writer.start();
+            reader.start();
+
+            writer.join();
+            reader.join();
+        } catch (IOException | InterruptedException e) {
+            System.out.println("Main Thread Exception: " + e.getMessage());
+        }
+    }
+}
